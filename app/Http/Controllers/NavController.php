b3ce05cdfbd14ea65b585737ab2e238486c2bcb3
Elias <mail@elias-dieter.de>
2023-05-07 15:12:04 +0200

    adding easy navbar router

---
+++ app/Http/Controllers/NavController.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class NavController extends Controller
+{
+    public function home(){
+        return view('welcome');
+    }
+    public function contact(){
+        return view('contact');
+    }
+}
