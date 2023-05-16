dca79451faf2b85d24f8cdfe8ae3aaa297c26315
Elias <mail@elias-dieter.de>
2023-05-16 20:30:45 +0200

    adding blog-entry uploads with imgs

--- admin.php
+++ admin.php
@@ -1,14 +1,28 @@
 <?php
-
-session_start();
-if(isset($_SESSION['loggedIn'])){
-    if($_SESSION['loggedIn'] == true){
-        echo "Willkommen Admin";
-    }
-    else{
-        echo "Bitte Anmelden";
-    }
-}
-else{
-    echo "Bitte Anmelden";
-}
+include('loginWatcher.php');
+        ?>
+            <!DOCTYPE html>
+            <html lang="en">
+                <head>
+                    <meta charset="UTF-8">
+                    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+                    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+                    <title>Admin</title>
+                </head>
+                <body>
+                    <input type="button" value="Neuer Beitrag" onclick="redirectToNewBlogEntry()">
+                    <input type="button" value="Beitrag Bearbeiten" onclick="redirectToEditBlogEntry()">
+                    <input type="button" value="Account verwalten" onclick="redirectToAccount()">
+                    <script>
+                        function redirectToNewBlogEntry() {
+                            window.location.href = "newBlogEntry.php";
+                        }
+                        function redirectToEditBlogEntry() {
+                            window.location.href = "editBlogEntries.php";
+                        }
+                        function redirectToAccount() {
+                            window.location.href = "account.php";
+                        }
+                    </script>
+                </body>
+            </html>
