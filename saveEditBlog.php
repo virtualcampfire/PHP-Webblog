235f9c4930db06e189299ff6efb16bc828dd9a35
Elias <mail@elias-dieter.de>
2023-05-21 12:31:06 +0200

    only the input, that changed will be saved

--- saveEditBlog.php
+++ saveEditBlog.php
@@ -3,21 +3,27 @@ $id = $_POST['id'];
 $title = $_POST['title'];
 $text = $_POST['text'];
 $image = $_FILES['image'];
+include('config.php');
+$db = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
 
-if (!empty($title) && !empty($text) && $image['error'] === 0) {
-    $uploadDir = 'blogImgs/';
-    $uploadedFilePath = $uploadDir . basename($image['name']);
-
-    if (move_uploaded_file($image['tmp_name'], $uploadedFilePath)) {
-        include('config.php');
-        $db = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
-        $sql = "UPDATE blog SET title = '$title', text = '$text', img_path = '$uploadedFilePath' WHERE id = '$id'";
+if(!empty($title)){
+    $sql = "UPDATE blog SET title = '$title' WHERE id = '$id'";
         $db->exec($sql);
-        header('Location: admin.php');
-        exit;
-    } else {
-        echo "File upload failed!";
-    }
-} else {
-    echo "Please fill in all the required fields and ensure the image upload was successful.";
 }
+if(!empty($text)){
+    $sql = "UPDATE blog SET text = '$text' WHERE id = '$id'";
+    $db->exec($sql);
+}
+if(pathinfo($image['name'], PATHINFO_EXTENSION) !== '' && pathinfo($image['name'], PATHINFO_EXTENSION) !== null){
+        $uploadDir = 'blogImgs/';
+        $uploadedFilePath = $uploadDir . basename($image['name']);
+        if(move_uploaded_file($image['tmp_name'], $uploadedFilePath)) {
+            $sql = "UPDATE blog SET img_path = '$uploadedFilePath' WHERE id = '$id'";
+            $db->exec($sql);
+        }
+        else {
+            echo "File upload failed!";
+        }
+}
+header('Location: admin.php');
+        exit;
